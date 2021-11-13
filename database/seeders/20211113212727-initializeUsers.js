1e32fede250eca9abeec048651aa83e49ff324a7
Maxi <herreramaxi@gmail.com>
2021-11-13 23:27:25 +0000

    adding sequelize. Adding login and users endpoints

---
+++ database/seeders/20211113212727-initializeUsers.js
@@ -0,0 +1,40 @@
+'use strict';
+var crypto = require('crypto');
+require('dotenv').config();
+const password = process.env.ADMIN_PWD;
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    //crypto.createHash('sha256').update(password).digest('hex');
+    // const hash = crypto.createHash('sha256').update(password).digest('hex');
+    const hash = crypto.createHash('sha256').update(password).digest('base64');
+    return queryInterface.bulkInsert('Users', [{
+      firstName: "admin",
+      lastName: "admin",
+      email: "admin@sf.com",
+      pass: hash,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const db = require('./database/models/index');
+const UserRepo = db.User;
+var crypto = require('crypto');
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded());
@@ -7,21 +10,86 @@ app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.get('/api/users', (req, res) => {
-    res.send("hello world!")
+if (process.env.NODE_ENV === 'production') {
+    var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+    const ignoreHosts = [/localhost:(\d{4})/];
+    // const ignoreHosts = [];
+    const ignoreRoutes = [];
+    app.use(redirectToHTTPS(ignoreHosts, ignoreRoutes));
+}
+
+app.get('/ping', (req, res) => {
+    res.send("pong")
+});
+
+app.post('/api/login', (req, res) => {
+
+    if (!(req.body.email && req.body.password)) {
+        return res.status(400).send({ message: "Mandatory fields not provided" });
+    }
+
+    UserRepo.findOne({ where: { email: req.body.email } }).then((h) => {
+        const hash = crypto.createHash('sha256').update(req.body.password).digest('base64');
+
+        if (!h || h.pass !== hash) {
+            return res.status(401).send("User credentials invalid");
+        }
+
+        res.send("ok");
+    }).catch(e => {
+        console.log(e);
+        res.status(500).send("Error on login endpoint");
+    });
 });
 
 app.post("/api/user", (req, res) => {
-    // if (!req.body.temperature)
-    //     return res.status(400).send({ message: "Mandatory fields not provided: [temperature]" });
+    if (!(req.body.firstName && req.body.lastName && req.body.email && req.body.password && req.body.data && req.body.hash)) {
+        return res.status(400).send({ message: "Mandatory fields not provided" });
+    }
 
     try {
 
+        const fieldsConcatenated = req.body.firstName + req.body.lastName + req.body.email + req.body.password + req.body.data;
+        const hash = crypto.createHash('sha256').update(fieldsConcatenated).digest('hex');
+
+        if (hash !== req.body.hash) {
+            return res.status(500).send("invalid hash");
+        }
+
+        console.log("firstName: " + req.body.firstName)
+        console.log("lastName: " + req.body.lastName)
+        console.log("email: " + req.body.email)
+        console.log("password: " + req.body.password)
         console.log("data: " + req.body.data)
-        console.log("pp: " + req.body.hash)
-        res.send("ok: " + req.body.data + ", " + req.body.hash);
+        console.log("hash: '" + req.body.hash + "'")
+        console.log("calculated hash: '" + hash + "'")
+        console.log("are equals: " + (hash === req.body.hash))
+
+
+
+        UserRepo.findOne({ where: { email: req.body.email } }).then((h) => {
+            if (h) {
+                h.firstName = req.body.firstName;
+                h.lastName = req.body.lastName;
+                h.email = req.body.email;
+                h.password = req.body.password;
+                h.data = req.body.data;
+                h.save();
+                res.send("User updated: " + h.email);
+            } else {
+                UserRepo.create({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: req.body.password,
+                    data: req.body.data
+                }).then(r => {
+                    res.send("User created: " + r.email);
+                });
+            }
+        });
     } catch (error) {
-        res.status(500).send({ message: "Error when trying to save a temperature sample. " + error })
+        res.status(500).send({ message: "Error when trying to create an user: " + error })
     }
 });
 // Start the app by listening on the default Heroku port
